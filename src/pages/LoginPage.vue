6a97ca5e7df87eefaf9d6b3c7d1dba3083a54b06
JHero <jhero@JHeros-MacBook-Air.local>
2025-03-30 11:25:19 -0500

    initialized repository

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page
+  class="flex flex-center"
+  padding
+  >
+  <div>
+    <h5 class="q-my-md text-center">
+      Login
+    </h5>
+    <q-card>
+      <q-card-section>
+        <q-input
+          label="email"
+          placeholder="email@example.com"
+          filled />
+
+        <q-input
+          label="password"
+          placeholder="password"
+          filled />
+      </q-card-section>
+      <q-btn
+        label=""
+    </q-card>
+  </div>
+</q-page>
+</template>
+
+<script setup>
+//
+</script>
